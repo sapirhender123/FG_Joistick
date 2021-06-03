40e600301520b3e8635add85f4b324436a415151
Ella Shalom <y.ariel@campus.technion.ac.il>
2021-06-04 00:16:15 +0300

    removed connectionRepository

--- app/src/main/java/com/fg_joystick/fg_joystick/ui/connect/view_model/ConnectionViewModelFactory.kt
+++ app/src/main/java/com/fg_joystick/fg_joystick/ui/connect/view_model/ConnectionViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.fg_joystick.fg_joystick.ui.connect.view_model
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.fg_joystick.fg_joystick.data.ConnectionDataSource
-import com.fg_joystick.fg_joystick.data.ConnectionRepository
 
 /**
  * ViewModel provider factory to instantiate connectViewModel.
@@ -15,9 +14,7 @@ class ConnectionViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ConnectionViewModel::class.java)) {
             return ConnectionViewModel(
-                connectionRepository = ConnectionRepository(
-                    dataSource = ConnectionDataSource()
-                )
+                connectionDataSource = ConnectionDataSource()
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
