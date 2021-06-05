3f75edee26a9c189e3293675d94f402d25f87a98
Sapir Hender <sapirhender@gmail.com>
2021-06-05 22:00:40 +0300

    Add comments

--- app/src/main/java/com/fg_joystick/fg_joystick/ui/connect/view/Joystick.kt
+++ app/src/main/java/com/fg_joystick/fg_joystick/ui/connect/view/Joystick.kt
@@ -1,9 +1,11 @@
 package com.fg_joystick.fg_joystick.ui.connect.view
 
+// Using strategy pattern
 interface OnJoystickChange {
     fun onChange(a: Float, e: Float)
 }
 
 class Joystick {
+    // Reusable Joystick
     var onChange: OnJoystickChange? = null
 }
